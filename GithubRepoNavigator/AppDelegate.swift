e3879216a37217d0476a16faa4fd065cd17d6185
howtojazz <swchowtojazz@gmail.com>
2017-12-10 11:07:15 +0900

    added main view controller

--- GithubRepoNavigator/AppDelegate.swift
+++ GithubRepoNavigator/AppDelegate.swift
@@ -15,7 +15,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = window else {
+            return true
+        }
+        
+        let mainVC = MainViewController()
+        mainVC.view.backgroundColor = UIColor.white
+        window.rootViewController = mainVC
+        mainVC.view.translatesAutoresizingMaskIntoConstraints = false
+        mainVC.view.leadingAnchor.constraint(equalTo: window.leadingAnchor, constant: 0.0)
+        mainVC.view.trailingAnchor.constraint(equalTo: window.trailingAnchor, constant: 0.0)
+        mainVC.view.topAnchor.constraint(equalTo: window.topAnchor, constant: 0.0)
+        mainVC.view.bottomAnchor.constraint(equalTo: window.bottomAnchor, constant: 0.0)
+        
+        
+        window.makeKeyAndVisible()
         return true
     }
 
